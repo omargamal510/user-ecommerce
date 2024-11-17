2088e65f9bf045746f3803237d253898bffb1d2e
omargamal510 <ogamal249@gmail.com>
2024-11-17 12:55:50 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RoutesWrapper from "./components/RoutesWrapper/RoutesWrapper";
 function App() {
   return (
     <>
-      {/* Update the router inside here */}
+      {/* Update the router inside here don't forget */}
       <Router>
         <RoutesWrapper />
       </Router>
